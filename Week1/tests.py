7bfc34853bd331df4a998cc694d4d5e89b183861
Herbert Turner <hmturner@mit.edu>
2019-10-02 22:15:41 -0400

    Daily commit

--- Week1/tests.py
+++ Week1/tests.py
@@ -2,6 +2,7 @@ from TLEReader import *
 
 testReader = TLEAnalyzer("./TLEs_Week1.txt","r")
 testReader.parse()
+print("Printing database \n")
 testReader.print()
 print("\n")
 testReader.print_Satellite_Info()
